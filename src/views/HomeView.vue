80bb06e8533f2c9d0a020b5fe8367c1c578515c1
dialbird <rememberthatk@icloud.com>
2022-05-19 13:20:59 +0900

    add tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="pt-20 w-full">
+    <div class="w-[500px] h-[600px] mx-auto rounded shadow-lg p-8">
+      <div>
+        <h1 class="text-3xl font-bold">
+          Hello world!
+        </h1>
+
+      </div>
+    </div>
   </main>
 </template>
