b219152023929a78101873f9fef7a974608da78f
dialbird <rememberthatk@icloud.com>
2022-05-20 10:07:10 +0900

    add dragevents

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import {useSortModelStore} from "@/stores/sortModelStore";
 import FormPopover from '@/views/FormPopover.vue'
 import {Popover, PopoverButton, PopoverPanel} from '@headlessui/vue'
 import {storeToRefs} from "pinia";
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
 import draggable from 'vuedraggable'
 
 subscribeSortModels((models) => {
@@ -18,9 +18,42 @@ defineComponent({
 const sortModelStore = useSortModelStore()
 const {sortModels} = storeToRefs(sortModelStore)
 
+const fromIdx = ref<number>()
+const toIdx = ref<number>()
+
+const onDragEnter = (e: DragEvent) => {
+  const target = e.target as HTMLElement
+  if (target.classList.contains('sort-item')) {
+    target.classList.remove('dragover')
+    target.classList.add('dragover')
+    toIdx.value = Number(target.dataset.idx)
+  }
+}
+const onDragLeave = (e: DragEvent) => {
+  const target = e.target as HTMLElement
+  if (target.classList.contains('sort-item')) {
+    target.classList.remove('dragover')
+  }
+}
+const onDragStart = (e: DragEvent) => {
+  const target = e.target as HTMLElement
+  fromIdx.value = Number(target.dataset.idx)
+}
 const onDragEnd = () => {
-  const len = sortModels.value.length
-  updateSortModels({ list: sortModels.value.map((sm, idx) => ({ ...sm, order: len - idx })) })
+  const lst = document.querySelectorAll('.sort-item')
+  for (var l of lst) {
+    l.classList.remove('dragover')
+  }
+  if (fromIdx.value !== undefined && toIdx.value !== undefined) {
+    const list = sortModels.value.slice()
+    const len = list.length
+    const moved = list[fromIdx.value]
+    list.splice(fromIdx.value, 1)
+    list.splice(toIdx.value, 0, moved)
+    updateSortModels({list: list.map((sm, idx) => ({...sm, order: len - idx}))})
+  }
+  fromIdx.value = undefined
+  toIdx.value = undefined
 }
 </script>
 
@@ -32,13 +65,20 @@ const onDragEnd = () => {
           List
         </h1>
         <ul>
-          <draggable v-model="sortModels" item-key="uid" @end="onDragEnd">
-            <template #item="{ element }">
-              <li class="border-b p-2 rounded-sm hover:bg-gray-100">
-                <p>{{ element.name }}</p>
-              </li>
-            </template>
-          </draggable>
+          <li
+            v-for="(sm, idx) in sortModels"
+            :key="sm.uid"
+            :data-idx="idx"
+            class="sort-item border-b p-2 rounded-sm hover:bg-gray-100"
+            draggable="true"
+            @dragstart="onDragStart"
+            @dragend.prevent="onDragEnd"
+            @dragenter.prevent="onDragEnter"
+            @dragleave.prevent="onDragLeave"
+            @dragover.prevent
+          >
+            <p class="select-none pointer-events-none">{{ sm.name }}</p>
+          </li>
           <li class="hover:bg-gray-100 transition relative">
             <Popover v-slot="{ close }">
               <PopoverButton class="w-full p-2 text-left">+ Add</PopoverButton>
@@ -51,7 +91,7 @@ const onDragEnd = () => {
                 leave-to-class="transform scale-95 opacity-0"
               >
                 <PopoverPanel class="absolute bg-white w-[200px] rounded shadow-md p-2">
-                  <FormPopover @submit-and-close="close()" />
+                  <FormPopover @submit-and-close="close()"/>
                 </PopoverPanel>
               </transition>
             </Popover>
@@ -61,3 +101,11 @@ const onDragEnd = () => {
     </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+.sort-item {
+  &.dragover {
+    background: rgba(87, 195, 215, 0.2);
+  }
+}
+</style>
